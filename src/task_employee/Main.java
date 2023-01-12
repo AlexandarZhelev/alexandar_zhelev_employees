0670a2f255e17ebfd796cda52537e37ef2219676
unknown <zhelev.alexandar@gmail.com>
2023-01-12 17:42:47 +0200

    edit

--- src/task_employee/Main.java
+++ src/task_employee/Main.java
@@ -55,68 +55,10 @@ public class Main {
         }
     }
 
-//    public static Long calculateDaysTogether(Employee emp1, Employee emp2){
-//        LocalDate startDay;
-//        LocalDate endDay;
-//        int start = emp1.getDateFrom().compareTo(emp2.getDateFrom());
-//
-//        if(start >= 0){
-//            startDay = emp2.getDateFrom();
-//        }
-//        else {
-//            startDay = emp1.getDateFrom();
-//        }
-//
-//        int end = emp1.getDateTo().compareTo(emp2.getDateTo());
-//        if(end >=0 ){
-//            endDay = emp2.getDateTo();
-//        }
-//        else {
-//            endDay = emp1.getDateTo();
-//        }
-//
-//        return ChronoUnit.DAYS.between(startDay, endDay);
-//    }
-
     CalculateDaysTogether daysTogether = new CalculateDaysTogether();
     static ReadCsvFile readCsvFile = new ReadCsvFile();
     CreateEmployee createEmployee; {
         createEmployee = new CreateEmployee();
     }
-//    private static List<Employee> readCsvFile(String file) {
-//        List<Employee>employeeList = new ArrayList<>();
-//        Path path = Paths.get(file);
-//
-//        try(BufferedReader bufferedReader = Files.newBufferedReader(path, StandardCharsets.UTF_8)){
-//            String line = bufferedReader.readLine();
-//
-//            while (line != null){
-//                String[] attributes = line.split(", ");
-//                Employee employee = createEmployee(attributes);
-//            }
-//        }
-//        catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return employeeList;
-//    }
 
-//    static Employee createEmployee(String[] attributes) {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
-//        Long empID = Long.parseLong(attributes[0]);
-//        Long projectID = Long.parseLong(attributes[1]);
-//        LocalDate dateFrom = LocalDate.parse(attributes[2], formatter);
-//        LocalDate dateTo;
-//
-//        if (attributes[3].equalsIgnoreCase("null")){
-//            dateTo = LocalDate.now();
-//        }
-//        else {
-//            dateTo = LocalDate.parse(attributes[3], formatter);
-//        }
-//
-//        return new Employee((long) Math.toIntExact(empID),
-//                (long) Math.toIntExact(projectID), dateFrom, dateTo);
-//    }
 }
